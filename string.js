007e4e461c5739214a643581bcb61bcd5728f313
BalacaYusuf <xamoyev.royal1998@gmail.com>
2023-07-21 14:26:30 +0300

    commit

---
+++ string.js
@@ -0,0 +1,197 @@
+
+
+// -------------------------------------TaskOne-----------------------------------------
+
+// let StringOrginal="hello";
+
+// let arrOne=[];
+// arrOne=StringOrginal.split('');
+
+// console.log( console.log(`Before reverse of array :${arrOne}`));
+
+
+
+// function riVerse(arr){
+
+//     arr.reverse();
+
+//     console.log(`After reverse of array :${arr}`);
+
+// }
+
+// riVerse(arrOne);
+
+
+// -------------------------------------TaskTwo-----------------------------------------
+
+
+// let FirstArray=[1,2,2,1];
+// let SecondArray=[2,2];
+// let PartnerArray=[];
+// function SameValue(arr1,arr2)
+// {
+//     arr1.forEach(element => {
+//        if( arr2.includes(element) ){
+//         PartnerArray.push(element);
+//        }
+//   });
+//   console.log(PartnerArray);
+// }
+
+// SameValue(FirstArray,SecondArray);
+
+
+// -------------------------------------TaskThree-----------------------------------------
+
+
+
+// let FirstArray=[1,2,3,1];
+// function TrueFalse(array)
+// {
+//    for(i=0;i<FirstArray.length;i++)
+//    {
+//     if(FirstArray.includes(FirstArray[i]))
+//     {
+// return true;
+//     }
+//     else
+//     {
+// return false;
+//     }
+//    }
+// }
+
+// console.log(TrueFalse(FirstArray));
+
+
+// -------------------------------------TaskFour-----------------------------------------
+
+
+
+//  let numeral=(prompt("Enter the Roman Numbers.").toUpperCase());
+// let numeral="VX";
+//  let RomanArray=[];
+//  RomanArray=numeral.split('');
+
+// console.log(RomanArray);
+// let result;
+// let sumofNumerals=0;
+
+// RomanArray.forEach(element => {
+    
+//     if(element=="I")
+//     {
+//     result=1;
+//     }
+// else if(element=="V")
+// {
+//     result=5;
+// }
+// else if(element=="X")
+// {
+//     result=10;
+// }
+// else if(element=="L")
+// {
+//     result=50;
+// }
+// else if(element=="C")
+// {
+//     result=100;
+// }
+// else if(element=="D")
+// {
+//     result=500;
+// }
+// else if(element=="M")
+// {
+//     result=100;
+// }
+// else
+// {
+// console.log("Please enter correct Roman numerals !");
+// }
+// console.log(result);
+// sumofNumerals=sumofNumerals+result;
+// });
+
+// console.log(sumofNumerals);
+// console.log(`You entered ${numeral} and it equals :${sumofNumerals}`);
+
+
+
+// -------------------------------------TaskFive-----------------------------------------
+
+
+// let arry=["ftknktljx","ftttx","fttjhfjfx"];
+// let str="";
+// let firstArray=(arry[0]).split('');
+// let firstArray1=(arry[1]).split('');
+// let firstArray2=(arry[2]).split('');
+
+
+
+
+
+// for(i=0;i<firstArray.length;i++)
+// {
+//   if((firstArray[i]==firstArray1[i])  &&(firstArray2[i]==firstArray[i]))
+//   {
+//     str+=firstArray[i];
+//   }
+// }
+//   console.log(str);
+
+
+// -------------------------------------TaskSix-----------------------------------------
+
+
+// let numbers =[2,2,38];
+
+
+
+// let lastnumber=(numbers[numbers.length-1]+1);
+
+
+
+//  numbers.splice(numbers.length-1,1,lastnumber);
+
+
+// console.log(numbers);
+
+  // -------------------------------------TaskSeven-----------------------------------------
+
+//   function test(n) {   
+//     if (n < 1) {
+//        return false;
+//      }
+//      while (n % 3 === 0) {
+//        n = n / 3;  
+//      }
+//      return n === 1;
+//    }
+//  console.log(test(1));
+
+
+
+// -------------------------------------TaskEight-----------------------------------------
+
+
+
+// let FirstArray=[1,2,3,1];
+// function TrueFalse(array)
+// {
+//    for(i=0;i<FirstArray.length;i++)
+//    {
+//     if(FirstArray.includes(FirstArray[i]))
+//     {
+// return true;
+//     }
+//     else
+//     {
+// return false;
+//     }
+//    }
+// }
+
+// console.log(TrueFalse(FirstArray));
